dc3c9cd14d7d92b1835d4adb06172f5b1dbafe24
Kevin Raimo <kevinraimolugo@gmail.com>
2021-10-23 19:29:04 -0300

    Jwt util added

---
+++ src/main/java/com/challengeWarmUp/challenge/util/JwtUtil.java
@@ -0,0 +1,55 @@
+package com.challengeWarmUp.challenge.util;
+
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.function.Function;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Service;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+
+@Service
+public class JwtUtil {
+
+  private static final String SECRET = "secret";
+  private static final int EXPIRATION = 3600000 * 12; // 12 hours
+
+  public String extractEmail(String token) {
+    return extractClaim(token, Claims::getSubject);
+  }
+
+  public Date extractExpiration(String token) {
+    return extractClaim(token, Claims::getExpiration);
+  }
+
+  public <T> T extractClaim(String token, Function<Claims, T> claimsResolver) {
+    final Claims claims = extractAllClaims(token);
+    return claimsResolver.apply(claims);
+  }
+
+  private Claims extractAllClaims(String token) {
+    return Jwts.parser().setSigningKey(SECRET).parseClaimsJws(token).getBody();
+  }
+
+  private Boolean isTokenExpired(String token) {
+    return extractExpiration(token).before(new Date());
+  }
+
+  public String generateToken(UserDetails userDetails) {
+    Map<String, Object> claims = new HashMap<>();
+    return createToken(claims, userDetails.getUsername());
+  }
+
+  private String createToken(Map<String, Object> claims, String subject) {
+    return Jwts.builder().setClaims(claims).setSubject(subject).setIssuedAt(new Date())
+        .setExpiration(new Date(System.currentTimeMillis() + EXPIRATION))
+        .signWith(SignatureAlgorithm.HS256, SECRET).compact();
+  }
+
+  public Boolean validateToken(String token, UserDetails userDetails) {
+    final String username = this.extractEmail(token);
+    return username.equals(userDetails.getUsername()) && !isTokenExpired(token);
+  }
+}
