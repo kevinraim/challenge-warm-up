85971dc24c7a2b7d6fa0ededef5bd5c477a178a5
Kevin Raimo <kevinraimolugo@gmail.com>
2021-10-24 11:30:59 -0300

    Authentication responose and request DTO added. New jwt filter and Jwt config

--- src/main/java/com/challengeWarmUp/challenge/controllers/UserController.java
+++ src/main/java/com/challengeWarmUp/challenge/controllers/UserController.java
@@ -2,29 +2,64 @@ package com.challengeWarmUp.challenge.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.challengeWarmUp.challenge.daoImp.User;
+import com.challengeWarmUp.challenge.dto.AuthRequest;
+import com.challengeWarmUp.challenge.dto.AuthResponse;
+import com.challengeWarmUp.challenge.security.UserDetailsServiceImp;
+import com.challengeWarmUp.challenge.util.JwtUtil;
 
 @RestController
 public class UserController {
-	
-	@Autowired
-	private User user;
-	
-	@PostMapping(value="auth/sign_up")
-	public  ResponseEntity<?> register(@RequestBody com.challengeWarmUp.challenge.models.User user){
-		
-		if(user.getEmail() == null || user.getEmail().isEmpty() || user.getPassword() == null || user.getEmail() == null)
-			return ResponseEntity.badRequest().body("Fields cannot be null or empty");
-		
-		if(this.user.findByEmail(user.getEmail()) != null) {
-			return ResponseEntity.badRequest().body("User alredy exists");
-		}
-		
-		this.user.create(user);
-		return ResponseEntity.ok("User has been register");
-	}
+
+  @Autowired
+  private User user;
+
+  @Autowired
+  private AuthenticationManager authManager;
+
+  @Autowired
+  private UserDetailsServiceImp userDetailsService;
+
+  @Autowired
+  private JwtUtil jwtUtil;
+
+  @PostMapping(value = "auth/log_in")
+  public ResponseEntity<?> createAuthenticationToken(@RequestBody AuthRequest authRequest)
+      throws Exception {
+
+    try {
+      authManager.authenticate(new UsernamePasswordAuthenticationToken(authRequest.getEmail(),
+          authRequest.getPassword()));
+    } catch (BadCredentialsException e) {
+      throw new Exception("Incorrect username or password", e);
+    }
+    final UserDetails userDetails =
+        this.userDetailsService.loadUserByUsername(authRequest.getEmail());
+    final String jwt = jwtUtil.generateToken(userDetails);
+
+    return ResponseEntity.ok(new AuthResponse(jwt));
+  }
+
+  @PostMapping(value = "auth/sign_up")
+  public ResponseEntity<?> register(@RequestBody com.challengeWarmUp.challenge.models.User user) {
+
+    if (user.getEmail() == null || user.getEmail().isEmpty() || user.getPassword() == null
+        || user.getEmail() == null)
+      return ResponseEntity.badRequest().body("Fields cannot be null or empty");
+
+    if (this.user.findByEmail(user.getEmail()) != null) {
+      return ResponseEntity.badRequest().body("User alredy exists");
+    }
+
+    this.user.create(user);
+    return ResponseEntity.ok("User has been register");
+  }
 }
---
+++ src/main/java/com/challengeWarmUp/challenge/filter/JwtFilter.java
@@ -0,0 +1,56 @@
+package com.challengeWarmUp.challenge.filter;
+
+import java.io.IOException;
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+import com.challengeWarmUp.challenge.security.UserDetailsServiceImp;
+import com.challengeWarmUp.challenge.util.JwtUtil;
+
+@Component
+public class JwtFilter extends OncePerRequestFilter {
+
+  @Autowired
+  private UserDetailsServiceImp userDetailsService;
+
+  @Autowired
+  private JwtUtil jwtUtil;
+
+  @Override
+  protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response,
+      FilterChain filterChain) throws ServletException, IOException {
+    final String authorizationHeader = request.getHeader("Authorization");
+
+    String username = null;
+    String jwt = null;
+
+    if (authorizationHeader != null && authorizationHeader.startsWith("Bearer ")) {
+      jwt = authorizationHeader.substring(7);
+      username = jwtUtil.extractEmail(jwt);
+    }
+
+    if (username != null && SecurityContextHolder.getContext().getAuthentication() == null) {
+      UserDetails userDetails = this.userDetailsService.loadUserByUsername(username);
+
+      if (jwtUtil.validateToken(jwt, userDetails)) {
+        UsernamePasswordAuthenticationToken usernamePassAuthToken =
+            new UsernamePasswordAuthenticationToken(userDetails, null,
+                userDetails.getAuthorities());
+
+        usernamePassAuthToken
+            .setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+        SecurityContextHolder.getContext().setAuthentication(usernamePassAuthToken);
+      }
+    }
+    filterChain.doFilter(request, response);
+  }
+
+}
