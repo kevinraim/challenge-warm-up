f1b468b39bf6fa8ffdf167521236b21a7549a02c
Kevin Raimo <kevinraimolugo@gmail.com>
2021-10-24 17:41:41 -0300

    User email unique=true added

--- src/main/java/com/challengeWarmUp/challenge/models/User.java
+++ src/main/java/com/challengeWarmUp/challenge/models/User.java
@@ -8,50 +8,50 @@ import javax.persistence.Id;
 import javax.persistence.Table;
 
 @Entity
-@Table(name="user", schema="public")
+@Table(name = "user", schema = "public")
 public class User {
-	
-	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	@Column(name="id")
-	private Long id;
-	
-	@Column(name="email")
-	private String email;
-	
-	@Column(name="password")
-	private String password;
-	
-	public User() {}
-	
-	public User(Long id, String email, String password) {
-		this.id = id;
-		this.email = email;
-		this.password = password;
-	}	
 
-	public Long getId() {
-		return id;
-	}
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  @Column(name = "id")
+  private Long id;
 
-	public void setId(Long id) {
-		this.id = id;
-	}
+  @Column(name = "email", unique = true)
+  private String email;
 
-	public String getEmail() {
-		return email;
-	}
+  @Column(name = "password")
+  private String password;
 
-	public void setEmail(String email) {
-		this.email = email;
-	}
+  public User() {}
 
-	public String getPassword() {
-		return password;
-	}
+  public User(Long id, String email, String password) {
+    this.id = id;
+    this.email = email;
+    this.password = password;
+  }
 
-	public void setPassword(String password) {
-		this.password = password;
-	}
+  public Long getId() {
+    return id;
+  }
+
+  public void setId(Long id) {
+    this.id = id;
+  }
+
+  public String getEmail() {
+    return email;
+  }
+
+  public void setEmail(String email) {
+    this.email = email;
+  }
+
+  public String getPassword() {
+    return password;
+  }
+
+  public void setPassword(String password) {
+    this.password = password;
+  }
 
 }
